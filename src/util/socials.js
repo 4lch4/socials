952472953d70e2d28be9ac53b80a6c149b416d32
4lch4 <inbox@4lch4.dev>
2021-04-17 12:09:30 -0500

    Revert "fix: updated links"
    This reverts commit 8db99cbab987016c92de4df08e47f11040c9e522.

--- src/util/socials.js
+++ src/util/socials.js
@@ -1,56 +1,56 @@
 const socials = [
   {
     headerImage: 'https://source.unsplash.com/pe_R74hldW4/1080x360',
-    avatarImage: '/socials/img/github.png',
+    avatarImage: '/img/github.png',
     name: 'GitHub',
     username: '4lch4',
     href: 'https://gh.4lch4.dev'
   },
   {
     headerImage: 'https://source.unsplash.com/uwPBAlQyIag/1080x360',
-    avatarImage: '/socials/img/discord.png',
+    avatarImage: '/img/discord.png',
     name: 'Discord',
     username: '4lch4#0042',
     href: 'https://discord.gg/Z9X7tXf8VR'
   },
   {
     headerImage: 'https://source.unsplash.com/pJ_DCj9KswI/1080x360',
-    avatarImage: '/socials/img/twitter.png',
+    avatarImage: '/img/twitter.png',
     name: 'Twitter',
     username: '4lch4',
     href: 'https://twitter.com/4lch4'
   },
   {
     headerImage: 'https://source.unsplash.com/pJ_DCj9KswI/1080x360',
-    avatarImage: '/socials/img/facebook.png',
+    avatarImage: '/img/facebook.png',
     name: 'Facebook',
     username: '4lch4',
     href: 'https://facebook.com/4lch4'
   },
   {
     headerImage: 'https://source.unsplash.com/pJ_DCj9KswI/1080x360',
-    avatarImage: '/socials/img/linkedin.png',
+    avatarImage: '/img/linkedin.png',
     name: 'LinkedIn',
     username: 'devin-leaman-49622429',
     href: 'https://linkedin.com/in/devin-leaman-49622429'
   },
   {
     headerImage: 'https://source.unsplash.com/pJ_DCj9KswI/1080x360',
-    avatarImage: '/socials/img/snapchat.png',
+    avatarImage: '/img/snapchat.png',
     name: 'Snapchat',
     username: 'dleaman',
     href: 'https://facebook.com/4lch4'
   },
   {
     headerImage: 'https://source.unsplash.com/2mjl2uvz9ic/1080x360',
-    avatarImage: '/socials/img/soundcloud.png',
+    avatarImage: '/img/soundcloud.png',
     name: 'Soundcloud',
     username: '4lch4',
     href: 'https://soundcloud.com/4lch4'
   },
   {
     headerImage: 'https://source.unsplash.com/GJ8ZQV7eGmU/1080x360',
-    avatarImage: '/socials/img/spotify.png',
+    avatarImage: '/img/spotify.png',
     name: 'Spotify',
     username: '4lch4',
     href:
@@ -58,14 +58,14 @@ const socials = [
   },
   {
     headerImage: 'https://source.unsplash.com/CSs8aiN_LkI/1080x360',
-    avatarImage: '/socials/img/twitch.png',
+    avatarImage: '/img/twitch.png',
     name: 'Twitch',
     username: '4lch4z',
     href: 'https://twitch.tv/4lch4z'
   },
   {
     headerImage: 'https://source.unsplash.com/CSs8aiN_LkI/1080x360',
-    avatarImage: '/socials/img/FM-White-Logo.png',
+    avatarImage: '/img/FM-White-Logo.png',
     name: 'E-Mail',
     username: '4lch4z',
     href: 'mailto://hey@4lch4.email'
